99759c6f9355e10f692655298b70f64e87dadb3f
Mohit Gupta <mohit.codes17@gmail.com>
2024-07-04 15:02:42 +0530

    add about section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import About from "@/components/sub-components/About";
 import Intro from "@/components/sub-components/Intro";
 
 const Home = () => {
   return (
     <div className="flex flex-col items-center px-4">
       <Intro />
+      <About />
     </div>
   );
 };
