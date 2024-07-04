47f5c61569aceffc8117b6c451801691cb3df924
Mohit Gupta <mohit.codes17@gmail.com>
2024-07-04 13:03:43 +0530

    add header

--- src/components/sub-components/Header.jsx
+++ src/components/sub-components/Header.jsx
@@ -1,5 +1,68 @@
+import { motion } from "framer-motion";
+
+const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Blog",
+    hash: "#blog",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+];
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="z-[999] relative">
+      <motion.div
+        className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[39rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
+        initial={{ y: -100, x: "-50%", opacity: 0 }}
+        animate={{ y: 0, x: "-50%", opacity: 1 }}
+        transition={{ duration: 0.25 }}
+      ></motion.div>
+
+      <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
+        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+          {links.map((link) => (
+            <motion.li
+              key={link.hash}
+              className="h-3/4 flex items-center justify-center"
+              initial={{ y: -100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ duration: 0.25, delay: 0.1 }}
+            >
+              <a
+                href={link.hash}
+                className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300"
+              >
+                {link.name}
+              </a>
+            </motion.li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -5,6 +5,11 @@ import { Outlet } from "react-router-dom";
 const Layout = () => {
   return (
     <>
+      <div className="bg-gray-50 text-gray-950 relative dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90">
+        <div className="bg-[#f5e2fb] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#d090e3d3]"></div>
+
+        <div className="bg-[#d7f1fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#84daf7c4]"></div>
+      </div>
       <Header />
       <Outlet />
       <Footer />
