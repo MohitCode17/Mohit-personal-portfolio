d321aca22f30dffab4f00aeaeef07a3b1e8f27eb
MohitCode17 <mohit.codes17@gmail.com>
2024-07-24 19:11:07 +0530

    add send message functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,22 @@ import Layout from "./layout/Layout";
 import Home from "./pages/Home";
 import SingleProject from "./pages/SingleProject";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="" element={<Home />} />
-          <Route path="project/:id" element={<SingleProject />} />
-        </Route>
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="" element={<Home />} />
+            <Route path="project/:id" element={<SingleProject />} />
+          </Route>
+        </Routes>
+      </Router>
+      <ToastContainer theme="dark" position="bottom-right" />
+    </>
   );
 };
 
--- src/components/sub-components/Contact.jsx
+++ src/components/sub-components/Contact.jsx
@@ -3,14 +3,47 @@ import { Input } from "../ui/input";
 import { Textarea } from "../ui/textarea";
 import { useState } from "react";
 import { FaPaperPlane } from "react-icons/fa";
+import { toast } from "react-toastify";
 
 const Contact = () => {
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    console.log("Submitted");
-  };
-
   const [loading, setLoading] = useState(false);
+  const [senderName, setSenderName] = useState("");
+  const [senderEmail, setSenderEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const res = await fetch(`http://localhost:8000/api/v1/message/send`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          senderName,
+          senderEmail,
+          subject,
+          message,
+        }),
+      });
+      setLoading(false);
+      const data = await res.json();
+
+      if (data.success === true) {
+        toast.success("Message sent successfully.");
+        setLoading(false);
+        setSenderName("");
+        setSenderEmail("");
+        setSubject("");
+        setMessage("");
+      }
+    } catch (error) {
+      setLoading(false);
+      toast.error(error.message);
+    }
+  };
 
   return (
     <motion.section
@@ -51,37 +84,39 @@ const Contact = () => {
         <div className="flex flex-col sm:flex-row items-center gap-2 mb-2">
           <Input
             className="h-12 px-4 text-white border-none rounded-lg bg-slate-700 bg-opacity-30 focus:bg-opacity-60 transition-all"
-            name="senderEmail"
-            type="email"
-            required
-            maxLength={500}
+            type="text"
             placeholder="Your name"
+            value={senderName}
+            onChange={(e) => setSenderName(e.target.value)}
+            required
           />
 
           <Input
             className="h-12 px-4 text-white border-none rounded-lg bg-slate-700 bg-opacity-30 focus:bg-opacity-60 transition-all"
-            name="senderEmail"
             type="email"
             required
-            maxLength={500}
             placeholder="Your email"
+            value={senderEmail}
+            onChange={(e) => setSenderEmail(e.target.value)}
           />
         </div>
         <Input
           className="h-12 px-4 text-white border-none rounded-lg bg-slate-700 bg-opacity-30 focus:bg-opacity-60 transition-all"
-          name="senderEmail"
-          type="email"
+          type="text"
           required
-          maxLength={500}
           placeholder="Subject"
+          value={subject}
+          onChange={(e) => setSubject(e.target.value)}
         />
         <Textarea
           className="mt-2 px-4 text-white border-none rounded-lg bg-slate-700 bg-opacity-30 focus:bg-opacity-60 transition-all resize-none"
-          name="senderEmail"
-          type="email"
+          name="message"
+          type="text"
           required
           maxLength={500}
           placeholder="Write your message here..."
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
         />
         <div className="flex items-center justify-center mt-4">
           <button
@@ -89,14 +124,8 @@ const Contact = () => {
             className="group flex items-center justify-center gap-2 h-[3rem] w-3/4 sm:w-1/2 bg-slate-700 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-slate-800 active:scale-105 dark:bg-opacity-40 disabled:scale-100 disabled:bg-opacity-65"
             disabled={loading}
           >
-            {loading ? (
-              <div className="h-5 w-5 animate-spin rounded-full border-b-2 border-white"></div>
-            ) : (
-              <>
-                Submit
-                <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />{" "}
-              </>
-            )}
+            Send Message
+            <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />{" "}
           </button>
         </div>
       </form>
